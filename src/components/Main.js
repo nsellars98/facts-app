dc25401cb5c91007bdaa8ece7a80e81704c09175
nsellars98 <nsellars98@gmail.com>
2021-11-18 23:05:08 -0600

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../style/style.css";
+
+function Main() {
+  return (
+    <h1>Main content goes here</h1>
+  );
+}
+
+export default Main;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../style/style.css";
+import icon from "../img/react-icon-small.png"
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src={icon} alt="React Logo" className="nav--icon"/>
+      <h3 className="nav--icon_text">ReactFacts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+}
+
+export default Navbar;
