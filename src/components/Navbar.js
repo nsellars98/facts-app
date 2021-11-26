9e3a76159c9a02eaa27c85d90b0c69158c8eea87
nsellars98 <nsellars98@gmail.com>
2021-11-26 12:43:11 -0600

    Taking steps to make project more responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "../style/style.css";
-import icon from "../img/react-icon-small.png";
+import icon from "../assets/img/react-icon-small.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <img src={icon} alt="React Logo" className="nav--icon"/>
+      <img src={icon} alt="Icon" className="nav--icon"/>
       <h3 className="nav--icon_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
