37d66873207f91aac3ac6a64aa946ab107d1e8b4
nsellars98 <nsellars98@gmail.com>
2021-12-15 15:34:47 -0600

    Code updates in source files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,14 @@
 import React from "react";
-import "../style/style.css";
 import icon from "../assets/img/react-icon-small.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <img src={icon} alt="icon" className="nav--icon"/>
+      <img 
+        src={icon} 
+        alt="React Logo" 
+        className="nav--icon"
+      />
       <h3 className="nav--icon_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./style/style.css";
 import App from "./App";
 
 const rootElement = document.querySelector("#root");
